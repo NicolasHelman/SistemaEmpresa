9b9af4babf8cbde3567ed3eecbff0a95fff28ea8
Nicolas Helman <nicolashelman11@gmail.com>
2022-06-23 23:33:35 -0300

    Config added

---
+++ config/APP.php
@@ -0,0 +1,10 @@
+<?php
+	const SERVERURL="http://localhost/SistemaEmpresa/";
+
+	const COMPANY="SISTEMA EMPRESARIAL";
+
+	const MONEDA="$";
+
+	date_default_timezone_set("America/Argentina/Buenos_Aires");
+
+?>
---
+++ config/SERVER.php
@@ -0,0 +1,14 @@
+<?php
+
+	const SERVER="localhost";
+	const DB="sistema-empresa";
+	const USER="root";
+	const PASS="root";
+
+	const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+	const METHOD="AES-256-CBC";
+	const SECRET_KEY='$SISTEMAEMPRESA@2022';
+	const SECRET_IV='210497';
+
+?>
